6d122e75315beafccb86dcf314d71224d87c9c51
Eyal Arubas <eyalarubas@gmail.com>
2018-05-02 20:12:07 +0100

    improve PendingOverlay component

--- src/ui/PendingOverlay/index.js
+++ src/ui/PendingOverlay/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react"
 import classnames from "classnames/bind"
+import { Intent, Spinner } from "@blueprintjs/core";
 
 import style from "./style.less"
 
 const cx = classnames.bind(style)
 
 const PendingOverlay = () => {
-  return <div>please wait...</div>
+  return <div className={cx("container")}>
+    <div className={cx("spinner")}>
+      <Spinner intent={Intent.PRIMARY}/>
+    </div>
+  </div>
 }
 
 PendingOverlay.displayName = "UI/PendingOverlay"
