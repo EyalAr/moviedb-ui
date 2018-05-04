21dd64684d82a1c09737d1fdfb595efc39b0435c
Eyal Arubas <eyalarubas@gmail.com>
2018-05-04 17:53:40 +0100

    api endpoints in config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,11 @@ const API_BASE = process.env.API_BASE || "https://api.themoviedb.org/3/"
 
 export default {
   endpoints: {
-    search: new URL("search/movie", API_BASE).href
-  }
+    search: new URL("search/movie", API_BASE).href,
+    configuration: new URL("configuration", API_BASE).href,
+    genres: new URL("genre/movie/list", API_BASE).href
+  },
+  imgSizeKey: 4, // the api can provide 7 images sizes, ranging 0-6
+  apiDateFormat: "YYYY-MM-DD", // api date format for movie release date
+  displayDateFormat: "MMMM Do YYYY"
 }
